566efac02188817b1ac1a2207ded5c1b1f9bfda5
goncmodev <gon.cmo.dev@gmail.com>
2021-06-05 21:14:37 +0100

    Validate the fields

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 // variables
 const btnEnviar = document.querySelector('#enviar');
 
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 
 // Eventos
 eventsListeners();
 function eventsListeners(){
+
+        // Cuando la app se carga ...
         document.addEventListener('DOMContentLoaded', iniciarApp);
+
+        // Campos del formulario
+        email.addEventListener('blur', validarFormulario);
+        asunto.addEventListener('blur', validarFormulario);
+        mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -13,4 +24,13 @@ function eventsListeners(){
 function iniciarApp() {
         btnEnviar.disabled = true;
         btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// validar formulario
+function validarFormulario(e) {
+        if(e.target.value.length > 0){
+                console.log('si hay algo');
+        }else{
+                e.target.classList.add('border', 'border-red-500');
+        }
 }
