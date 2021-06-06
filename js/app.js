468fb8e77d85de4adda80880f84b48a8ee5f3717
goncmodev <gon.cmo.dev@gmail.com>
2021-06-06 07:28:55 +0100

    Clean error message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
@@ -32,5 +33,19 @@ function validarFormulario(e) {
                 console.log('si hay algo');
         }else{
                 e.target.classList.add('border', 'border-red-500');
+
+                mostrarError();
+        }
+}
+
+function mostrarError(){
+        const mensajeError = document.createElement('p');
+        mensajeError.textContent = 'Todos los campos son obligatorios';
+        mensajeError.classList.add('border', 'border-red-500', 'background-color-100', 'text-red-500', 
+                                        'p-3', 'mt-5', 'text-center', 'error');
+
+        const errores = document.querySelectorAll('.error');
+        if(errores.length === 0){
+                formulario.appendChild(mensajeError);
         }
 }
